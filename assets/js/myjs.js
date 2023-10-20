c3db363171b80a4f125fbdc0fb6fd867b511dac5
Zivhd <zivhd.dev@outlook.com.vn>
2023-10-20 13:01:42 +0700

    add whitelist

--- assets/js/myjs.js
+++ assets/js/myjs.js
@@ -25,20 +25,29 @@ function depositTab(tabId) {
 
 
 // check website 
-// Lấy URL hiện tại
 var currentURL = window.location.href;
 
 // Danh sách các tên miền hợp lệ
-var validDomains = ["robuxvn.net", "rodmin.vn" ,"127.0.0.1:5500"];
+var validDomains = ["robuxvn.net", "rodmin.vn", "127.0.0.1:5500"];
 
 // Kiểm tra xem currentURL chứa một trong các tên miền hợp lệ
-var isValidDomain = validDomains.some(function(domain) {
+var isValidDomain = validDomains.some(function (domain) {
     return currentURL.indexOf(domain) !== -1;
 });
 
-if (isValidDomain) {         
+if (isValidDomain) {
     // Tên miền hợp lệ, không cần chuyển hướng
     console.log("Design by Zivhd | Contact:  Zivhd.Info");
 } else {
-     window.location.href = "https://facebook.com/zivhd.username";
+    // Hiển thị thông báo và yêu cầu người dùng nhập key
+    var userInput = prompt("Nhập key để tiếp tục:");
+    
+    // Kiểm tra key
+    if (userInput === "your_secret_key") { // Thay "your_secret_key" bằng key thực tế của bạn
+        // Key đúng, không cần chuyển hướng
+        console.log("Design by Zivhd | Contact:  Zivhd.Info");
+    } else {
+        // Key sai, chuyển hướng sang link Facebook của bạn
+        window.location.href = "https://facebook.com/zivhd.username";
+    }
 }
